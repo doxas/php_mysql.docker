619c876674790be05a668736a84b5e40b1dbb42c
doxas <eizanco@gmail.com>
2019-12-16 20:09:57 +0900

    add lib.php

--- src/index.php
+++ src/index.php
@@ -12,27 +12,13 @@
             <p>php_mysql</p>
         </div>
 <?php
-    $mysql = new mysqli(
-        $_ENV['DATABASE_HOST'],
-        $_ENV['MYSQL_USER'],
-        $_ENV['MYSQL_PASSWORD'],
-        $_ENV['MYSQL_DATABASE']
-    );
-    if(!$mysql){
-        echo "Error: Unable to connect to MySQL." . PHP_EOL;
-        echo "Debugging error: " . mysqli_connect_error() . PHP_EOL;
-        exit;
-    }
-    $sql = "INSERT INTO hoges(created_at) VALUES('" . date('Y-m-d H:i:s') . "')";
-    $result = $mysql->query($sql);
-    $sql = "SELECT * FROM hoges ORDER BY hoge_id desc limit 1";
-    $result = $mysql->query($sql)->fetch_row();
-    echo '<pre class="log">';
-    var_dump($result);
-    echo '</pre>';
-    mysqli_close($mysql);
+include_once(dirname(__FILE__).'/lib/lib.php');
 
-    phpinfo();
+echo '<pre class="log">';
+var_dump(getFromMySQL());
+echo '</pre>';
+
+phpinfo();
 ?>
     </body>
 </html>
